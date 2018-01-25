fc2ef66ecf9b9045a09a6d5308b20612c5f0729b
CodyPerakslis <CodyPerakslis@noreply.github.com>
2018-01-24 22:51:26 -0600

    umncloud

--- deployment/load_variables.sh
+++ deployment/load_variables.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Kanetus
-HeadBucket="www.kanetus.com"
-WebTarget="kanetus"
+#HeadBucket="www.kanetus.com"
+#WebTarget="kanetus"
 
 # Personal
 #HeadBucket="www.codyperakslis.com"
 #WebTarget="codyperakslis"
+
+# Cloud Computing
+HeadBucket="www.umncloud.com"
+WebTarget="umncloud"
